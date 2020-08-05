f3db8fa35827fd461f9a8856284e2bc80a3115aa
FR Rumman <fazla.rumman@northsouth.edu>
2020-08-05 23:06:35 +0600

    lab 5 practice

---
+++ cse lab 5 practice/lab 5 p3.c
@@ -0,0 +1,17 @@
+
+
+#include<stdio.h>
+int main()
+{
+    int i, times;
+    scanf("%d", &times);
+    i=0;
+    do
+    {
+        i++;
+        printf("%d\n", i);
+
+    }
+     while (i <= times);
+    return 0;
+}
